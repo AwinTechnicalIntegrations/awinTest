9e7293f430b6fa089540bc5c55e9d0c80967586f
AwinTechnicalIntegrations <99187202+AwinTechnicalIntegrations@users.noreply.github.com>
2022-02-08 11:29:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -28,15 +28,23 @@ function checkTestType(){
 }
 
 function addTestType1(){
-    _url = location.href;
-    _url += (_url.split('?')[1] ? '&':'?') + "testType=TagManager";
-    location.href = _url;    
+    var queryString = window.location.search;
+    var urlParams = new URLSearchParams(queryString);
+    if(urlParams.get("testType") != "TagManager"){
+        _url = location.href;
+        _url += (_url.split('?')[1] ? '&':'?') + "testType=TagManager";
+        location.href = _url; 
+    }       
 }
 
 function addTestType2(){
-    _url = location.href;
-    _url += (_url.split('?')[1] ? '&':'?') + "testType=CodeIntegration";
-    location.href = _url;
+    var queryString = window.location.search;
+    var urlParams = new URLSearchParams(queryString);
+    if(urlParams.get("testType") != "TagManager"){
+        _url = location.href;
+        _url += (_url.split('?')[1] ? '&':'?') + "testType=CodeIntegration";
+        location.href = _url;
+    }   
 }
  
 function checkMID(){
