a44e18b726c6d40420ec2674d2727ab9843042b3
Allan Urique <31394670+southrad69@users.noreply.github.com>
2022-03-28 11:09:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -27,6 +27,22 @@ function checkTestType(){
     return testTypeParameterValue;
 }
 
+function GetAWcookie(name){
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
 function successPageType(){
     if(checkTestType() == "TagManager"){
         location.href = "https://stupefied-perlman-581078.netlify.app/success.html?testType=TagManager";
